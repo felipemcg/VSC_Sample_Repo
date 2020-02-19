e6d23db4d7520f6c5497dd5b312d7be8c76db09e
Felipe Costa <felipe.ce.n@hotmail.com>
2020-02-19 10:59:13 +0100

    Removed some code

--- script.py
+++ script.py
@@ -1,18 +1,6 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-print ("Hellooow")
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
-print("MBO")
-
+print(r.code)
